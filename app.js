9bc9f1cb40ede2d510ff9f0019bfc4836fafd681
bypcarpark <byp.carpark@gmail.com>
2022-08-11 16:40:24 +0700

    เรียกใช้งาน Express Session

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 // Import Express
 const express = require("express");
 
+//Import Express Flash
+const flash = require("express-flash");
+
+//Import Express Session
+const session = require("express-session");
+
 // Import EJS Layout
 const expressLayouts = require("express-ejs-layouts");
 
 // Import Router frontend.js
 const frontendRouter = require("./routes/frontend");
 // Import Router backend.js
-const backendRouter = require('./routes/backend')
-
+const backendRouter = require("./routes/backend");
 
 // Create express object
 const app = express();
@@ -21,10 +26,27 @@ app.use(expressLayouts);
 app.set("layout", "./layouts/frontend");
 app.set("view engine", "ejs");
 
+//กำหนดค่าให้สามารถรับค่าจากฟอร์มได้
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//เรียกใช้งาน Express Session
+app.use(
+  session({
+    cookie: { macAge: 60000 },
+    store: new session.MemoryStore(),
+    saveUninitialized: true,
+    resave: "true",
+    secret: "secret",
+  })
+);
+
+//เรียกใช้งาน Express flash
+app.use(flash());
+
 // เรียกใช้งาน Routes
 app.use("/", frontendRouter);
-app.use('/backend', backendRouter)
-
+app.use("/backend", backendRouter);
 
 // Run Express Server ที่ Port 5000
 app.listen(5000, () => {
