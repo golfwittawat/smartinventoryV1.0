7959393d20d72970f3431ecc9d21d989b4289a30
bypcarpark <byp.carpark@gmail.com>
2022-08-18 16:55:28 +0700

    Edit_category

--- routes/backend.js
+++ routes/backend.js
@@ -4,6 +4,9 @@ const express = require("express");
 // Import Moment เพื่อไว้จัดรูปแบบวันที่
 const moment = require("moment");
 
+//Import OjectID ของMongoDB
+const objectId = require("mongodb").ObjectId;
+
 const router = express.Router();
 
 // Import mongodb_dbconfig
@@ -22,7 +25,11 @@ router.get("", (req, res) => {
 // CRUD Category ================================================
 // Read Category อ่านดาต้าเบส
 router.get("/category", async (req, res) => {
-  const category = await db.collection("category").find({}).toArray();
+  const category = await db
+    .collection("category")
+    .find({})
+    .sort({ CategoryID: -1 })
+    .toArray();
   //   res.json(category);
 
   res.render("pages/backend/category", {
@@ -45,8 +52,16 @@ router.get("/create_category", (req, res) => {
 
 // Create Category POST
 router.post("/create_category", async (req, res) => {
+  //การอ่านหมายเลข CategoryID ล่าสุด
+  const category = await db
+    .collection("category")
+    .findOne({}, { sort: { CategoryID: -1 }, limit: 1 }); //เลขเรียงจากมากไปน้อย เอามา1 รายการ
+  const categoryID = category.CategoryID + 1;
+  //   console.log(category.CategoryID); //แสดงค่าเลข ID ตัวสุดท้าย
+  //   return 0;//รันคำสั่งสุดท้าย แทนการกด//
+
   // รับค่าจากฟอร์ม
-  let CategoryID = req.body.CategoryID;
+  //   let CategoryID = req.body.CategoryID; //ถ้าสร้างรับค่สตัวเลขสุดท้ายและ+1 มาแล้วไม่ต้องสร้าง
   let CategoryName = req.body.CategoryName;
   let CategoryStatus = req.body.CategoryStatus;
   let curdatetime = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
@@ -54,7 +69,7 @@ router.post("/create_category", async (req, res) => {
 
   // console.log(CategoryID+CategoryName+CategoryStatus)
   // Validate ฟอร์มว่าป้อนข้อมูลครบหรือยัง
-  if (CategoryID === "" || CategoryName.length === 0 || CategoryStatus === "") {
+  if (CategoryName.length === 0 || CategoryStatus === "") {
     errors = true;
     // แสดงข้อความแจ้งเตือน
     req.flash("error", "ป้อนข้อมูลในฟิลด์ให้ครบก่อน");
@@ -67,7 +82,7 @@ router.post("/create_category", async (req, res) => {
   } else {
     // Insert to mongodb
     await db.collection("category").insertOne({
-      CategoryID: parseInt(CategoryID),
+      CategoryID: categoryID,
       CategoryName: CategoryName,
       CategoryStatus: parseInt(CategoryStatus),
       CreatedDate: curdatetime,
@@ -86,11 +101,18 @@ router.post("/create_category", async (req, res) => {
 });
 
 // Edit Category
-router.get("/edit_category", (req, res) => {
+router.get("/edit_category/:id", async (req, res) => {
+  const objID = new objectId(req.params.id);
+  const category = await db
+    .collection("category")
+    .find({ _id: objID })
+    .toArray();
+
   res.render("pages/backend/edit_category", {
     title: "Edit Category",
     heading: "Edit Category",
     layout: "./layouts/backend",
+    data: category,
   });
 });
 
